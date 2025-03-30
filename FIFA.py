13bf19dd8820fb0f1bb99e0e15f482c9d5d551b5
taleibhans <132792914+taleibhans@users.noreply.github.com>
2025-03-30 18:54:49 -0400

    FIFA.py

--- FIFA.py
+++ FIFA.py
@@ -123,4 +123,4 @@ def update_winner_info(selected_year):
 
 # Run app (Updated Fix)
 if __name__ == '__main__':
-    app.run_server(debug=True, host="0.0.0.0", port=10000)
+    app.run(debug=True, host="0.0.0.0", port=10000)
