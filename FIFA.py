9863b501263c1be47d9cfcd10d371eca848509b5
taleibhans <132792914+taleibhans@users.noreply.github.com>
2025-03-30 19:20:05 -0400

    Update FIFA.py

--- FIFA.py
+++ FIFA.py
@@ -48,5 +48,4 @@ app.layout = html.Div([
 
 # Run app (Test version)
 if __name__ == '__main__':
-    app.run_server(debug=True, host="0.0.0.0", port=10000)
-
+    app.run(debug=True, host="0.0.0.0", port=10000)
